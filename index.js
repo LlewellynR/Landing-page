869a4abfc8537e203ba273cc89ecf758cd8b106f
ryl <ryl12@aber.ac.uk>
2025-11-23 16:34:43 +0000

    Changed the CSS to resemble a Marquee

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-function showMenu(){
+function showMenu() {
     let menu = document.getElementById("menu");
-    if(menu.style.opacity === "1"){
+    if (menu.style.opacity === "1") {
         menu.style.opacity = "0";
-    }else if(menu.style.opacity === "0"){
+    } else if (menu.style.opacity === "0") {
         menu.style.opacity = "1";
     }
 
 }
+
